1a3d767e5ed8d783a4e7c22475d314fd308520e1
lordvcs <diabolicfreak@gmail.com>
2025-01-13 11:43:00 +0530

    feat: add python extras

--- nvim/.config/nvim/lua/plugins/extras.lua
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -1,4 +1,5 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
 }
