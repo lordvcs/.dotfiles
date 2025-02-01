7d69fe8db1f0c6d78b43187513b1d05f45ecce3a
lordvcs <diabolicfreak@gmail.com>
2025-02-01 11:32:01 +0530

    feat: add mini surround and docker extras

--- nvim/.config/nvim/lua/plugins/extras.lua
+++ nvim/.config/nvim/lua/plugins/extras.lua
@@ -2,5 +2,7 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
 	{ import = "lazyvim.plugins.extras.lang.markdown" },
 	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
 	{ import = "lazyvim.plugins.extras.editor.snacks_picker" },
+	{ import = "lazyvim.plugins.extras.coding.mini-surround" },
 }
