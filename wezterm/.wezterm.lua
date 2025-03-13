ff0b8fa308582d3ec6d589b18031b3abc3a353f3
lordvcs <diabolicfreak@gmail.com>
2025-03-14 00:41:50 +0530

    feat: add copymode default keybindings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -51,6 +51,255 @@ end
 -- config.send_composed_key_when_left_alt_is_pressed = false
 -- config.send_composed_key_when_right_alt_is_pressed = false
 
+default_copy_mode_keys = {
+	{ key = "Tab", mods = "NONE", action = wezterm.action.CopyMode("MoveForwardWord") },
+	{
+		key = "Tab",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveBackwardWord"),
+	},
+	{
+		key = "Enter",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToStartOfNextLine"),
+	},
+	{
+		key = "Escape",
+		mods = "NONE",
+		action = wezterm.action.Multiple({
+			{ CopyMode = "MoveToViewportBottom" },
+			{ CopyMode = "Close" },
+		}),
+	},
+	{
+		key = "Space",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Cell" }),
+	},
+	{
+		key = "$",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToEndOfLineContent"),
+	},
+	{
+		key = "$",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToEndOfLineContent"),
+	},
+	{ key = ",", mods = "NONE", action = wezterm.action.CopyMode("JumpReverse") },
+	{ key = "0", mods = "NONE", action = wezterm.action.CopyMode("MoveToStartOfLine") },
+	{ key = ";", mods = "NONE", action = wezterm.action.CopyMode("JumpAgain") },
+	{
+		key = "F",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ JumpBackward = { prev_char = false } }),
+	},
+	{
+		key = "F",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode({ JumpBackward = { prev_char = false } }),
+	},
+	{
+		key = "G",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToScrollbackBottom"),
+	},
+	{
+		key = "G",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToScrollbackBottom"),
+	},
+	{ key = "H", mods = "NONE", action = wezterm.action.CopyMode("MoveToViewportTop") },
+	{
+		key = "H",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToViewportTop"),
+	},
+	{
+		key = "L",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToViewportBottom"),
+	},
+	{
+		key = "L",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToViewportBottom"),
+	},
+	{
+		key = "M",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToViewportMiddle"),
+	},
+	{
+		key = "M",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToViewportMiddle"),
+	},
+	{
+		key = "O",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToSelectionOtherEndHoriz"),
+	},
+	{
+		key = "O",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToSelectionOtherEndHoriz"),
+	},
+	{
+		key = "T",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ JumpBackward = { prev_char = true } }),
+	},
+	{
+		key = "T",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode({ JumpBackward = { prev_char = true } }),
+	},
+	{
+		key = "V",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Line" }),
+	},
+	{
+		key = "V",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Line" }),
+	},
+	{
+		key = "^",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToStartOfLineContent"),
+	},
+	{
+		key = "^",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode("MoveToStartOfLineContent"),
+	},
+	{ key = "b", mods = "NONE", action = wezterm.action.CopyMode("MoveBackwardWord") },
+	{ key = "b", mods = "ALT", action = wezterm.action.CopyMode("MoveBackwardWord") },
+	{ key = "b", mods = "CTRL", action = wezterm.action.CopyMode("PageUp") },
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action.Multiple({
+			{ CopyMode = "MoveToViewportBottom" },
+			{ CopyMode = "Close" },
+		}),
+	},
+	{
+		key = "d",
+		mods = "CTRL",
+		action = wezterm.action.CopyMode({ MoveByPage = 0.5 }),
+	},
+	{
+		key = "e",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveForwardWordEnd"),
+	},
+	{
+		key = "f",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ JumpForward = { prev_char = false } }),
+	},
+	{ key = "f", mods = "ALT", action = wezterm.action.CopyMode("MoveForwardWord") },
+	{ key = "f", mods = "CTRL", action = wezterm.action.CopyMode("PageDown") },
+	{
+		key = "g",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToScrollbackTop"),
+	},
+	{
+		key = "g",
+		mods = "CTRL",
+		action = wezterm.action.Multiple({
+			{ CopyMode = "MoveToViewportBottom" },
+			{ CopyMode = "Close" },
+		}),
+	},
+	{ key = "h", mods = "NONE", action = wezterm.action.CopyMode("MoveLeft") },
+	{ key = "j", mods = "NONE", action = wezterm.action.CopyMode("MoveDown") },
+	{ key = "k", mods = "NONE", action = wezterm.action.CopyMode("MoveUp") },
+	{ key = "l", mods = "NONE", action = wezterm.action.CopyMode("MoveRight") },
+	{
+		key = "m",
+		mods = "ALT",
+		action = wezterm.action.CopyMode("MoveToStartOfLineContent"),
+	},
+	{
+		key = "o",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToSelectionOtherEnd"),
+	},
+	{
+		key = "q",
+		mods = "NONE",
+		action = wezterm.action.Multiple({
+			{ CopyMode = "MoveToViewportBottom" },
+			{ CopyMode = "Close" },
+		}),
+	},
+	{
+		key = "t",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ JumpForward = { prev_char = true } }),
+	},
+	{
+		key = "u",
+		mods = "CTRL",
+		action = wezterm.action.CopyMode({ MoveByPage = -0.5 }),
+	},
+	{
+		key = "v",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Cell" }),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Block" }),
+	},
+	{ key = "w", mods = "NONE", action = wezterm.action.CopyMode("MoveForwardWord") },
+	{
+		key = "y",
+		mods = "NONE",
+		action = wezterm.action.Multiple({
+			{ CopyTo = "ClipboardAndPrimarySelection" },
+			{ CopyMode = "MoveToViewportBottom" },
+			{ CopyMode = "Close" },
+		}),
+	},
+	{ key = "PageUp", mods = "NONE", action = wezterm.action.CopyMode("PageUp") },
+	{ key = "PageDown", mods = "NONE", action = wezterm.action.CopyMode("PageDown") },
+	{
+		key = "End",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToEndOfLineContent"),
+	},
+	{
+		key = "Home",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveToStartOfLine"),
+	},
+	{ key = "LeftArrow", mods = "NONE", action = wezterm.action.CopyMode("MoveLeft") },
+	{
+		key = "LeftArrow",
+		mods = "ALT",
+		action = wezterm.action.CopyMode("MoveBackwardWord"),
+	},
+	{
+		key = "RightArrow",
+		mods = "NONE",
+		action = wezterm.action.CopyMode("MoveRight"),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT",
+		action = wezterm.action.CopyMode("MoveForwardWord"),
+	},
+	{ key = "UpArrow", mods = "NONE", action = wezterm.action.CopyMode("MoveUp") },
+	{ key = "DownArrow", mods = "NONE", action = wezterm.action.CopyMode("MoveDown") },
+}
+
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
 	local tab, pane, window = mux.spawn_window(cmd or {})
@@ -170,40 +419,46 @@ config.keys = {
 		action = wezterm.action.SendString("\x0e"), -- This sends Ctrl+P with Shift modifier
 	},
 }
+
 -- COPY MODE --start
-config.key_tables = {
-	copy_mode = {
-		{ key = "Escape", mods = "NONE", action = wezterm.action({ CopyMode = "Close" }) },
-		{ key = "h", mods = "NONE", action = wezterm.action({ CopyMode = "MoveLeft" }) },
-		{ key = "j", mods = "NONE", action = wezterm.action({ CopyMode = "MoveDown" }) },
-		{ key = "k", mods = "NONE", action = wezterm.action({ CopyMode = "MoveUp" }) },
-		{ key = "l", mods = "NONE", action = wezterm.action({ CopyMode = "MoveRight" }) },
-		{
-			key = "V",
-			mods = "SHIFT",
-			action = wezterm.action.CopyMode({ SetSelectionMode = "Line" }),
-		},
-		{
-			key = "v",
-			mods = "NONE",
-			action = wezterm.action.CopyMode({ SetSelectionMode = "Cell" }),
-		},
-		{
-			key = "y",
-			mods = "NONE",
-			action = wezterm.action.Multiple({
-				{ CopyTo = "ClipboardAndPrimarySelection" },
-				wezterm.action.ClearSelection,
-				wezterm.action.CopyMode("ClearSelectionMode"),
-			}),
-		},
-		-- Enter search mode to edit the pattern.
-		-- When the search pattern is an empty string the existing pattern is preserved
-		{ key = "/", mods = "NONE", action = wezterm.action({ Search = { CaseSensitiveString = "" } }) },
-		-- navigate any search mode results
-		{ key = "n", mods = "NONE", action = wezterm.action({ CopyMode = "NextMatch" }) },
-		{ key = "N", mods = "SHIFT", action = wezterm.action({ CopyMode = "PriorMatch" }) },
+custom_copy_mode_keys = {
+	{ key = "Escape", mods = "NONE", action = wezterm.action({ CopyMode = "Close" }) },
+	{ key = "h", mods = "NONE", action = wezterm.action({ CopyMode = "MoveLeft" }) },
+	{ key = "j", mods = "NONE", action = wezterm.action({ CopyMode = "MoveDown" }) },
+	{ key = "k", mods = "NONE", action = wezterm.action({ CopyMode = "MoveUp" }) },
+	{ key = "l", mods = "NONE", action = wezterm.action({ CopyMode = "MoveRight" }) },
+	{
+		key = "V",
+		mods = "SHIFT",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Line" }),
 	},
+	{
+		key = "v",
+		mods = "NONE",
+		action = wezterm.action.CopyMode({ SetSelectionMode = "Cell" }),
+	},
+	{
+		key = "y",
+		mods = "NONE",
+		action = wezterm.action.Multiple({
+			{ CopyTo = "ClipboardAndPrimarySelection" },
+			wezterm.action.ClearSelection,
+			wezterm.action.CopyMode("ClearSelectionMode"),
+		}),
+	},
+	-- Enter search mode to edit the pattern.
+	-- When the search pattern is an empty string the existing pattern is preserved
+	{ key = "/", mods = "NONE", action = wezterm.action({ Search = { CaseSensitiveString = "" } }) },
+	-- navigate any search mode results
+	{ key = "n", mods = "NONE", action = wezterm.action({ CopyMode = "NextMatch" }) },
+	{ key = "N", mods = "SHIFT", action = wezterm.action({ CopyMode = "PriorMatch" }) },
+}
+
+for i, key_entry in ipairs(custom_copy_mode_keys) do
+	default_copy_mode_keys[#default_copy_mode_keys + 1] = key_entry
+end
+config.key_tables = {
+	copy_mode = default_copy_mode_keys,
 	search_mode = {
 		{ key = "Escape", mods = "NONE", action = wezterm.action({ CopyMode = "Close" }) },
 		-- Go back to copy mode when pressing enter, so that we can use unmodified keys like "n"
@@ -212,8 +467,6 @@ config.key_tables = {
 	},
 }
 -- COPY MODE --end
--- config.enable_kitty_keyboard = true
--- config.allow_win32_input_mode = false
 
 for i = 0, 9 do
 	-- leader + number to activate that tab
